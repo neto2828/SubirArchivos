579c970e0ebb6a4582e4a42eb4d8a78b2f9a1fa7
Néstor <Nstor@DESKTOP-1ST4C0D>
2022-10-05 00:13:01 -0500

    Agregar archivos de proyecto.

---
+++ SubirArchivos/Controllers/ArchivoController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNetCore.Mvc;
+using SubirArchivos.Models.ViewModels;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
+
+namespace SubirArchivos.Controllers
+{
+    public class ArchivoController : Controller
+    {
+
+        private readonly IHostEnvironment _host;
+
+        public ArchivoController(IHostEnvironment host)
+        {
+            _host = host;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Save(ArchivoViewModel model)
+        {
+            string RutaSitio = _host.ContentRootPath;
+            //string PathArchivo1 = Path.Combine(RutaSitio+"/Files/arhivo1.png");
+            //string PathArchivo2 = Path.Combine(RutaSitio + "/Files/arhivo2.png");           
+                if (!ModelState.IsValid)
+            {
+                return View("Index");
+            }
+
+            using (Stream fileStream1 = new FileStream(Path.Combine(RutaSitio + "/Files/arhivo1.png"), FileMode.Create))
+            {
+                model.Archivo1.CopyToAsync(fileStream1);
+            }
+
+            using (Stream fileStream2 = new FileStream(Path.Combine(RutaSitio + "/Files/arhivo2.png"), FileMode.Create))
+            {
+                model.Archivo2.CopyToAsync(fileStream2);
+            }
+
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ SubirArchivos/Models/ViewModels/ArchivoViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+ 
+namespace SubirArchivos.Models.ViewModels
+{
+    public class ArchivoViewModel
+    {
+        [Required]
+        [DisplayName("Archivo 1")]
+        public IFormFile? Archivo1 {get;set;}
+        [Required]
+        [DisplayName("Archivo 2")]
+        public IFormFile? Archivo2 { get; set; }
+        [Required]
+        [DisplayName("Cadena")]
+        public string? Cadena { get; set; }
+    }
+}
